cdb3f9f007eca50ee39da520cd07f82982581317
Ziv Levy <levi.ziv@gmail.com>
2019-04-13 14:35:52 +0300

    fix actions issue

--- examples/TodoApp/src/components/Todo/todoActions.js
+++ examples/TodoApp/src/components/Todo/todoActions.js
@@ -1,24 +1,14 @@
 import { ADD_ITEM, SET_INPUT_VISIBILITY, TOGGLE_ITEM } from './todoActionTypes'
 
 const addItem = itemText => ({ type: ADD_ITEM, data: { key: itemText, isDone: false } })
-
+const toggleItemDone = item => ({ type: TOGGLE_ITEM, data: { item } })
 const setInputVisibility = isVisible => ({ type: SET_INPUT_VISIBILITY, data: isVisible })
 
 const handleAddButtonClick = () => setInputVisibility(true)
 
 const handleAddTodoSubmission = itemText => async dispatch => {
   dispatch(setInputVisibility(false))
-  await getSomething()
   dispatch(addItem(itemText))
-}
-
-function a(itemText) {
-  return function b(dispatch) {}
-}
-const toggleItemDone = item => ({ type: TOGGLE_ITEM, data: { item } })
-
-function c(item) {
-  return { type: TOGGLE_ITEM, data: { item } }
 }
 
 export default {
