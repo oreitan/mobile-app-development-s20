fe10d6c90248b8c738cc49cddcd7b182b1291fab
Ziv Levy <levi.ziv@gmail.com>
2019-04-14 20:21:00 +0300

    initial todo list component tests

--- examples/TodoApp/__tests__/TodoList.test.js
+++ examples/TodoApp/__tests__/TodoList.test.js
@@ -1,20 +1,50 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import 'react-native'
 import React from 'react'
-import { TodoList } from '../src/components/Todo/TodoList'
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
+import { TodoList } from '../src/components/Todo/TodoList'
+import TodoListItem from '../src/components/Todo/TodoListItem'
 
-it('renders correctly', () => {
-  const items = [
-    { key: 'Laundry', isDone: false },
-    { key: 'Shopping', isDone: false },
-    { key: 'Call lawyer', isDone: false }
-  ]
-  const testRenderer = renderer.create(<TodoList items={items} showAddItemInput={false} />)
+describe('TodoList tests', function() {
+  test('renders correctly', async () => {
+    const items = [
+      { key: 'Laundry', isDone: false },
+      { key: 'Shopping', isDone: false },
+      { key: 'Call lawyer', isDone: false }
+    ]
+
+    const testRenderer = renderer.create(<TodoList items={items} showAddItemInput={false} />)
+    expect(testRenderer.root.props).toHaveProperty('items')
+    expect(testRenderer.root.props.items).toHaveLength(3)
+  })
+
+  test('renders todo items correctly', async () => {
+    const items = [
+      { key: 'Laundry', isDone: false },
+      { key: 'Shopping', isDone: false },
+      { key: 'Call lawyer', isDone: false }
+    ]
+
+    const testRenderer = renderer.create(<TodoList items={items} showAddItemInput={false} />)
+    expect(testRenderer.root.findAllByType(TodoListItem)).toHaveLength(items.length)
+  })
+
+  test('toggle item on press', async () => {
+    const items = [
+      { key: 'Laundry', isDone: false },
+      { key: 'Shopping', isDone: false },
+      { key: 'Call lawyer', isDone: false }
+    ]
+
+    const onToggle = jest.fn()
+
+    const testRenderer = renderer.create(
+      <TodoList items={items} showAddItemInput={false} toggleItemDone={onToggle} />
+    )
+    const todoItemComponent = testRenderer.root.findAllByType(TodoListItem)[0]
+
+    todoItemComponent.props.onPress()
+
+    expect(onToggle).toHaveBeenCalledTimes(1)
+    expect(onToggle).toHaveBeenCalledWith({ key: todoItemComponent.props.label, isDone: false })
+  })
 })
